5127fa56bd9ff3799a02ba777e51fc966b35b875
syn3r6y <syn3rgy@live.ca>
2016-12-14 15:30:31 -0700

    roughs in date prompt selector

--- app/components/calendar-prompt/date-prompt.js
+++ app/components/calendar-prompt/date-prompt.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import {
   View,
+  StyleSheet,
 } from 'react-native';
+import ButtonPrompt from './button-prompt';
+import TextPrompt from './text-prompt';
 
 class DatePrompt extends React.Component {
 
   render() {
+    let styles = StyleSheet.create({
+      width: 0,
+      opacity: 0,
+      height: 0,
+      position: 'absolute',
+      top: 0,
+      zIndex: 10,
+    });
+
     return (
       <View>
-
+        <ButtonPrompt />
+        <TextPrompt />
+        <ButtonPrompt />
       </View>
     );
   }
-
 }
 
 export default DatePrompt;
